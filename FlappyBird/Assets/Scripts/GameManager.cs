a29a98bc6d40b9ae26888dc693cd497b46dedc57
Hicanos <ruda32168@gmail.com>
2025-04-29 19:50:26 +0900

    feat; 게임 종료, 재시작, 점수
    게임 종료 및 재시작, 점수표시

--- FlappyBird/Assets/Scripts/GameManager.cs
+++ FlappyBird/Assets/Scripts/GameManager.cs
@@ -1,14 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     static GameManager gameManager;
     public static GameManager Instance { get { return gameManager; } }
 
+    private int currentScore = 0;
+
+    UIManager uiManager;
+    public UIManager UIManager { get { return uiManager; } }
+
     private void Awake()
     {
         gameManager = this;
+        uiManager = FindObjectOfType<UIManager>();
+    }
+
+    private void Start()
+    {
+        uiManager.UpdateScore(0);
+    }
+
+    public void GameOver()
+    {
+        uiManager.SetRestart();
+    }
+
+    public void RestartGame()
+    {
+        //지금 실행중인 씬의 이름을 가져와서 로드함
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void AddScore(int score)
+    {
+        currentScore += score;
+        uiManager.UpdateScore(currentScore);
     }
 }
