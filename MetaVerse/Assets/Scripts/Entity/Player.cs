d8be3bbbba6675a43b184b5519d457ddd8ea11b0
Hicanos <ruda32168@gmail.com>
2025-05-06 23:06:16 +0900

    feat; 씬 변경하는 포탈 생성(수정중)/ add; StartScene
    추가된 내용
    1. 스타트 씬 생성.
    2. 씬 변경 포탈 생성 _ 현재 씬을 옮겨갈 때마다 캐릭터를 생성하도록 해야함.
    3. bug - 문 상호작용 시 Order in Layer가 복구되지않음

--- MetaVerse/Assets/Scripts/Entity/Player.cs
+++ MetaVerse/Assets/Scripts/Entity/Player.cs
@@ -3,29 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Player : InteractionManager
+public class Player : MonoBehaviour
 {
-    public bool CanInteract;
-
-    //태그가 설정된 오브젝트가 상호작용 가능한 오브젝트라면 = CanInteract true
-    private GameObject interactObj { get; set; }
-
-    public override void Interact()
-    {
-
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        //게임오브젝트의 태그가 Interact라면, 상호작용 가능.
-        if (collision.CompareTag("Interact"))
-        {
-            CanInteract = true;            
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        CanInteract = false;
-    }
+   
 }
--- MetaVerse/Assets/Scripts/Objects/DoorInteract.cs
+++ MetaVerse/Assets/Scripts/Objects/DoorInteract.cs
@@ -1,70 +1,67 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DoorInteract : InteractionManager
 {
     bool isOpen = false;
-    public GameObject Door;
+    public Collider2D Door;
+    public GameObject Collider;
     public Player player;
 
 
     //상호작용 가능한 것이 옆에 있으면, F키 누를 수 있도록 활성화 - Player의 불리언 CanInteract = true라면 상호작용 가능 상태
     //문이 열리면 Indoor의 콜라이더를 Trigger로 변경
     //문이 열리면 애니메이션도 작동해야함
+    //현재: 애니메이터- 작동함
+    //문제: 콜라이더 접촉은 됨. 지금 F눌렀을 때 Interact가 제대로 안 먹힘
 
 
 
+    [SerializeField]
     protected Animator animator;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        Door = GetComponent<Collider2D>();
     }
     public override void Interact()
     {
-        if (player.CanInteract==true && Input.GetKeyDown(KeyCode.F))
-        {
-            if (isOpen == false)
-            {
-                isOpen = true;
-                animator.SetBool("IsOpen", true);
-                //문을 여는 애니메이션 작동
+        Debug.Log("문 열렸니?");
+        Collider2D DoorCollider = Collider.GetComponent<Collider2D>();
+        //콜라이더가 존재하고 열려있으면 트리거를 켜고, 콜라이더가 없고 닫혀있으면 콜라이더를 끈다.
 
-            }
-            else if (isOpen == true)
-            {
-                isOpen = false;
-                animator.SetBool("IsOpen", false);
-            }
-            ChangeColliderTrigger();
+        if (DoorCollider != null && isOpen == false) //닫힌 문을 열어야함
+        {
+            isOpen = true;
+            animator.SetBool("IsOpen", true);
+            DoorCollider.isTrigger = true;
+            Debug.Log("문 열었다");
+            //문을 여는 애니메이션 작동
+
         }
-
-    }
-
-    void ChangeColliderTrigger()
-    {
-        if(Door != null)
+        else if (isOpen == true) //열린 문 닫아야함
         {
-            Collider2D DoorCollider = Door.GetComponent<Collider2D>();
-            if (DoorCollider != null && isOpen ==true)
-            {
-                DoorCollider.isTrigger = true;
-            }
-            else if (DoorCollider != null && isOpen ==false)
-            {
-                DoorCollider.isTrigger = false;
-            }
-            else
-            {
-                Debug.LogWarning(Door.name + "콜라이더 컴포넌트를 찾을 수 없습니다.");
-            }
+            isOpen = false;
+            animator.SetBool("IsOpen", false);
+            DoorCollider.isTrigger = false;
+            //문을 닫는 애니메이션 작동
+            Debug.Log("문 닫앗다");
         }
         else
         {
-            Debug.LogWarning("대상 오브젝트를 찾을 수 없습니다.");
+            Debug.LogWarning(Collider.name + "콜라이더 컴포넌트를 찾을 수 없습니다.");
+
         }
     }
+
+
+
+
+
+
 }
 
